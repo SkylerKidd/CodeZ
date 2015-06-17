e73504b6f3a05491813aaecbc11f54655d6e961a
SkylerKidd <f.s.kidd@gmail.com>
2015-06-17 16:46:23 -0700

    DS - Updating comments

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -5,12 +5,15 @@ version = '1.0'
 # Intro Statement
 print 'Hello, and welcome to Dice Simulator v' + version
 
+# Checks if user input was a valid format
 def validInput(input):
     return True #pass
 
+# Roll and print result
 def diceRoll(input):
     pass
 
+# Main loop
 while(True):
     print 'Please input the value to want to roll.'
     print 'Format: #d# where # is an int, 0 to 20'
@@ -19,3 +22,4 @@ while(True):
     if not validInput(input):
         print 'Invalid input.'
         continue
+    diceRoll(input)
