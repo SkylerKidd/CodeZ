6d9ff47933ab71f2b8e32f4b7df330e5471083d9
SkylerKidd <f.s.kidd@gmail.com>
2015-06-17 17:03:18 -0700

    DS - Redo logic

--- DiceSimulator.py
+++ DiceSimulator.py
@@ -1,29 +1,28 @@
-import random
+import random, sys
 
 version = '1.0'
 
 # Intro Statement
 print 'Hello, and welcome to Dice Simulator v' + version
+print 'Please input the value to want to roll.'
 
 # Checks if user input was a valid format
 def validInput(dVal):
+    # Check for validity of command
     return True #pass
 
 # Roll and print result
 def diceRoll(dVal):
     pass
 
-# Checks if user wants to play again
-def playAgain():
-
-
 # Main loop
 while(True):
-    print 'Please input the value to want to roll.'
-    print 'Format: #d# where # is an int, 0 to 20'
     dVal = input('Input: ')
+    if dVal == 'exit':
+        sys.exit()
+    if dVal == 'help':
+        print 'Format: #d# where # is an int, 0 to 20'
     if not validInput(dVal):
-        print 'Invalid input.'
+        print 'Please input a valid command. (\'help\' for format)'
         continue
     diceRoll(dVal)
-    playAgain()
