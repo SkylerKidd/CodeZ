7375e460cd767e9a11246bb77cb2b7d0dffb891c
Michael Henning <myklhenn@icloud.com>
2015-07-22 13:24:57 -0700

    Updated ball.py

--- Breakout/ball.py
+++ Breakout/ball.py
@@ -18,5 +18,15 @@ class Ball(object):
         #(color is green)
         self.color = (73,232,74)
 
+    #update ball position
+    def update(self):
+        self.x += self.x_vel
+        self.y += self.y_vel
+
+    #update ball velocity
+    def accelerate(self, x_multipier, y_multiplier):
+        self.x_vel *= x_multiplier
+        self.y_vel *= y_multiplier
+
     def draw(self, screen):
 		pygame.draw.circle(screen, self.color, (self.x, self.y), self.r, 0)
