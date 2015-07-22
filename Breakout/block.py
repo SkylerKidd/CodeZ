71df8c3bde2e62f599099b3fc3daf259242f7cf6
Skyler Kidd <f.s.kidd@gmail.com>
2015-07-22 15:49:29 -0700

    Block shadow

--- Breakout/block.py
+++ Breakout/block.py
@@ -1,7 +1,7 @@
 import pygame
 
 class Block(object):
-    def __init__(self, screen_width, screen_height, x, y):
+    def __init__(self, screen_width, screen_height, x, y, color, bkg_color):
         self.screen_width = screen_width
         self.screen_height = screen_height
         self.width, self.height = 48, 20
@@ -10,7 +10,11 @@ class Block(object):
         self.y = y
 
         self.rect = (self.x, self.y, self.width, self.height)
-        self.color = (232,160,73)
+        self.shadow_rect = (self.x+3, self.y+3, self.width, self.height)
+
+        self.color = color
+        self.shadow_color = bkg_color
 
     def draw(self, screen):
 		pygame.draw.rect(screen, self.color, self.rect, 0)
+        pygame.draw.shadow_rect(screen, self.shadow_color, self.rect, 0)
